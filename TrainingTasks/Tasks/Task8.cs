1bfb5b4c9b64741b0a129b6513946e1efd1fb7ad
Hedydy <sasha.lukashow@gmail.com>
2020-12-07 00:56:20 +0200

    Task 8(2)

--- TrainingTasks/Tasks/Task8.cs
+++ TrainingTasks/Tasks/Task8.cs
@@ -1,29 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TrainingTasks
 {
-
     /*
         Задача: Дан массив целых чисел. Вывести максимальную сумму элементов в массиве. 
                 Суммировать элементы можно только последовательно.
-        Пример: [-1, 10, -9, 5, 6, -10]
+        Пример: [-1, 10, -9, 5, 6, -10],
         Вывод: 12
+        Пример: [1, 5, 7, -20, 3, 100, -250, 88, 33, 1, -40, 120] 
+        Вывод: 202
+        Пример: [1, 3, -8, 3, -1, 2, 1] 
+        Вывод: 5
     */
 
     class Task8
     {
-        public static void DoTask8_v1(int[] array)
+        public static void DoTask8_v1(params int[] array)
         {
-            int sum = 0;
+            int currentSum = 0;
+            int lastSum = 0;
 
             for (int i = 0; i < array.Length; i++)
             {
-                    
+                if(array[i] > 0)
+                {
+                    currentSum += array[i];
+
+                    if(currentSum < 0)
+                    {
+                        currentSum = array[i];
+                    }
+
+                    lastSum = currentSum;
+                }
+                else if(currentSum >= 0)
+                {
+                    currentSum += array[i];
+
+                    if(currentSum < 0 && i > 0)
+                    {
+                        currentSum = 0;
+                    }
+                }
             }
+
+            Console.WriteLine(currentSum > lastSum ? currentSum : lastSum);
         }
     }
 }
